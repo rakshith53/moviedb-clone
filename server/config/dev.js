68c98e9b2b9d2bb9ce5260b2e3b950307b1a424c
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-02-11 11:07:19 +0530

    Add files via upload

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Rakshith:rakshith@cluster0.ycpqd.mongodb.net/movie?retryWrites=true&w=majority",
+};
+//mongodb+srv://Rakshith:rakshith@cluster0-ycpqd.mongodb.net/test?retryWrites=true&w=majority
+//mongodb+srv://Rakshith:rakshith@cluster0-ycpqd.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb+srv://Rakshith:rakshith@cluster0-ycpqd.mongodb.net/test
